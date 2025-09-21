c8c827fe869bb133bff6148d289ab39552aff037
Fabrizio <fabriziocaricato1@gmail.com>
2025-09-21 02:36:47 -0300

    componente de cancion con estructura inicial creado

---
+++ src/components/Cancion.jsx
@@ -0,0 +1,10 @@
+export default function Cancion() {
+  return (
+    <div >
+      <span>▶</span>
+      <h1>Cancion 1</h1>
+      <small>Artista</small>
+      <span>3:42</span>
+    </div>
+  )
+}
